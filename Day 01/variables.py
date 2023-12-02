012bb605910632b8609df97d38146d4175dc4938
Sivaprasad. G <sivaprasad.g@live.com>
2023-12-02 12:27:47 +0530

    Day 1 finished

---
+++ Day 01/variables.py
@@ -0,0 +1,19 @@
+# Example Input 1
+# 29
+# 41
+# Example Output 1
+# a: 41
+# b: 29
+ 
+# There are two variables, a and b from input
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+ # b is now equal to the original value of a
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
